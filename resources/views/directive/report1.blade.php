af79582cb30628d854512cf974e2ef3f0072e337
Rafael Martínez Méndez <martinez_brg21@protonmail.com>
2017-12-28 17:23:04 -0600

    Reporte1 Corregido

--- resources/views/directive/report1.blade.php
+++ resources/views/directive/report1.blade.php
@@ -8,55 +8,5 @@
         </div>
     </div>
 </div>
-@php
-$q1a=10;
-$q1b=20;
-
-@endphp
-
-
-    <script src="/js/highcharts.js"></script>
-    <script src="/js/exporting.js"></script>
-    <script type="text/javascript">
-        Highcharts.chart('container', {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: null,
-            type: 'pie'
-        },
-        title: {
-            text: 'Campus Norte '
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.y:.0f} puntos </b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                },
-                showInLegend: true
-            }
-        },
-        series: [{
-            name: 'Puntos Encuesta',
-            colorByPoint: true,
-            data: [{
-                name: 'Si',
-                y: {{$q1a}}
-            }, {
-                name: 'No',
-                y: {{$q1b}}
-            }]
-        }]
-        });
-    </script>
 
 @endsection
--- resources/views/layouts/directive.blade.php
+++ resources/views/layouts/directive.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="/css/sidebar.css">
 
 </head>
-<body>
+<body id="wrapper">
     <div class="wrapper"  id="app">
           <!-- Sidebar Holder -->
             <nav id="sidebar">
@@ -66,13 +66,62 @@
 
                 </div>
 
-             
+                 
                 @yield('content')
             </div>     
     </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <script src="/js/highcharts.js"></script>
+    <script src="/js/exporting.js"></script>
+    <script type="text/javascript">
+
+            @php
+        $q1a=10;
+        $q1b=20;
+    @endphp
+        Highcharts.chart('container', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: null,
+            type: 'pie'
+        },
+        title: {
+            text: 'Campus Norte '
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.y:.0f} puntos </b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Puntos Encuesta',
+            colorByPoint: true,
+            data: [{
+                name: 'Estudiantes No Encuestados',
+                y: {{$q1a}}
+            }, {
+                name: 'Estudiantes Encuestados',
+                y: {{$q1b}}
+            }]
+        }]
+        });
+    </script>
     <script type="text/javascript">
              $(document).ready(function () {
                  $('#sidebarCollapse').on('click', function () {
