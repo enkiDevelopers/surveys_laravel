81bdad2a59efce87a251e653e0f994c829914b63
Jorge Gonzalez <colocho-2104@hotmail.com>
2018-01-16 14:08:15 -0600

    correccion consulta

--- app/Http/Controllers/editController.php
+++ app/Http/Controllers/editController.php
@@ -11,14 +11,13 @@ class editController extends Controller
 {
   public function busqueda($id)
   {
-        $cTitulo = DB::table('surveys')->select(['Titulo_encuesta'])->where('id', $id)->get();
-      $cDescripcion = DB::table('surveys')->select(['Descripcion'])->where('id', $id)->get();
-      $cNombre = DB::table('surveys')->select(['Image_path'])->where('id', $id)->get();
 
-        $titulo = $cTitulo[0]->Titulo_encuesta;
-        $descripcion = $cDescripcion[0]->Descripcion;
-        $nombre = $cNombre[0]->Image_path;
 
-    return view("administrator.edit",compact('titulo','descripcion','nombre'));
+      $consulta = DB::table('surveys')->select(['Titulo_encuesta','Descripcion','Image_path'] )->where('id', $id)->get();
+      $titulo = $consulta[0]->Titulo_encuesta;
+      $descripcion = $consulta[0]->Descripcion;
+      $nombre = $consulta[0]->Image_path;
+
+        return view("administrator.edit",compact('titulo','descripcion','nombre'));
   }
 }
