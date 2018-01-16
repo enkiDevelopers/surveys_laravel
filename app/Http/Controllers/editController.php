8a9353e251e6528bf9f9b670cbc3202bd97e2c20
Jorge Gonzalez <colocho-2104@hotmail.com>
2018-01-16 14:00:42 -0600

    enviar a edit

--- app/Http/Controllers/editController.php
+++ app/Http/Controllers/editController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use  DB;
 use App\surveys;
-
+use Response;
 
 class editController extends Controller
 {
   public function busqueda($id)
   {
-      $titulo = DB::table('surveys')->select(['Titulo_encuesta'])->where('id', $id)->get();
-      $descripcion = DB::table('surveys')->select(['Descripcion'])->where('id', $id)->get();
-      $nombre = DB::table('surveys')->select(['Image_path'])->where('id', $id)->get();
+        $cTitulo = DB::table('surveys')->select(['Titulo_encuesta'])->where('id', $id)->get();
+      $cDescripcion = DB::table('surveys')->select(['Descripcion'])->where('id', $id)->get();
+      $cNombre = DB::table('surveys')->select(['Image_path'])->where('id', $id)->get();
 
-      return $titulo. $descripcion. $nombre;
-      return view("administrator.edit",compact('titulo','descripcion','nombre'));
+        $titulo = $cTitulo[0]->Titulo_encuesta;
+        $descripcion = $cDescripcion[0]->Descripcion;
+        $nombre = $cNombre[0]->Image_path;
+
+    return view("administrator.edit",compact('titulo','descripcion','nombre'));
   }
 }
