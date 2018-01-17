dae44d9136f7ac51ef2209376ae7c6180de05fb0
rafaelMMGH <martinez_brg21@protonmail.com>
2018-01-17 16:38:28 -0600

    TC10 Templates

--- database/migrations/2017_12_26_213746_create_templates_table.php
+++ database/migrations/2017_12_26_213746_create_templates_table.php
@@ -1,40 +1,37 @@
 <?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-
-class CreateQuestionsTemplatesTable extends Migration {
-
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('templates', function(Blueprint $table)
-		{
-			$table->Increments('idTemplates');	  
-			$table->string('name', 45)->nullable();
-			$table->string('description', 45)->nullable();
-			$table->date('dateIni')->nullable();
-			$table->date('dateEnd')->nullable();
-			$table->integer('administrators_idAdministrators')->index('fk_idAdministrators_administrators1_idx'); 	
-			$table->timestamps();
-
-
+ 
+ use Illuminate\Database\Migrations\Migration;
+ use Illuminate\Database\Schema\Blueprint;
+ 
+ class CreateTemplatesTable extends Migration {
+ 
+ 	/**
+ 	 * Run the migrations.
+ 	 *
+ 	 * @return void
+ 	 */
+ 	public function up()
+ 	{
+ 		Schema::create('templates', function(Blueprint $table)
+ 		{
+ 			$table->integer('idTemplates')->primary();
+ 			$table->string('name', 45)->nullable();
+ 			$table->string('description', 45)->nullable();
+ 			$table->date('dateIni')->nullable();
+ 			$table->date('dateEnd')->nullable();
+ 			$table->integer('administrators_idAdministrators')->index('fk_templates_administrators1_idx');
  		});
-	}
-
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('questionsTemplates');
-	}
-
-}
+ 	}
+ 
+ 
+ 	/**
+ 	 * Reverse the migrations.
+ 	 *
+ 	 * @return void
+ 	 */
+ 	public function down()
+ 	{
+ 		Schema::drop('templates');
+ 	}
+ 
+ }
