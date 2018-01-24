1c0d158d58541b80f186e2c3622f0ef275f6a8e9
Juan Carlos Lopez Pimentel <jclopezpimentel@gmail.com>
2018-01-24 16:24:10 -0600

    editController descompuesto
    Jorge arreglará editController

--- app/Http/Controllers/editController.php
+++ app/Http/Controllers/editController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\templates2s;
+use App\templates;
 use App\questionstemplates;
 use Illuminate\Http\Request;
 use  DB;
@@ -14,8 +14,11 @@ class editController extends Controller
   public function busqueda($id)
   {
       $consulta = DB::table('templates')->select(['tituloEncuesta','descripcion','imagePath','creador'] )->where('id', $id)->get();
+<<<<<<< HEAD
       //$questions = DB::table('questionstemplates')->select(['tituloEncuesta','descripcion','imagePath','creador'] )->where('idTemplates', $id)->get();
       //$questions=DB::select('SELECT COUNT(idQuestionsTemplates) AS numpreg FROM questionstemplates where templates_idTemplates=?;', [$id]);
+=======
+>>>>>>> ca09097ec11ecc1ca9eecb2091fca6c073c3bcad
 
       $titulo = $consulta[0]->tituloEncuesta;
 
@@ -32,15 +35,16 @@ class editController extends Controller
 
       return view("administrator.edit",compact('titulo','descripcion','nombre','eid','datos','admor'));
   }
-  
-  public function delete($id)
+
+  public function delete($id,$idadmin)
   {
 
-    $post =templates2s::where('id',$id)->first();
+
+    $post =templates::where('id',$id)->first();
       $post->delete();
 
 
-    return Redirect("administrator/surveys");
+    return Redirect("administrator/surveys/".$idadmin);
 
   }
 }
