47cb1c039a017206aa94b5266af1d43ff757edc4
Jorge Gonzalez <colocho-2104@hotmail.com>
2018-02-09 16:47:54 -0600

    Plantilla e-mail

--- resources/views/administrator/borrar.blade.php
+++ resources/views/administrator/borrar.blade.php
@@ -1,37 +1,83 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>UVM | Universidad del Valle de México </title>
-    <style media="screen">
-      #content
-      {
-        position: relative;
-        margin-top: 100px;
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Encues</title>
+</head>
+<body style="background-color: black ">
+<!--Copia desde aquí-->
+<table style="max-width: 600px; padding: 10px; margin:0 auto; border-collapse: collapse;">
+	<tr>
+		<td style="background-color: #ecf0f1; text-align: left; padding: 0">
+				<img width="20%" style="display:block; margin: 1.5% 3%" height="80px" src="https://image.ibb.co/eXzEyx/mail_logo.png">
+		</td>
+	</tr>
+	<tr>
+		<td style="padding: 0">
+			<img style="padding: 0; display: block" src="https://image.ibb.co/fmTvOx/self_assessment.jpg" width="100%">
+		</td>
+	</tr>
 
-        overflow: auto;
-        word-break: break-all;
-        -webkit-hyphens: auto;
-        -moz-hyphens: auto;
-        hyphens: auto;
-        width: 80%;
-        height: 380px;
+	<tr>
+		<td style="background-color: #ecf0f1">
+			<div style="margin: 4% 10% 2%; text-align: justify;font-family: sans-serif">
+				<h2 style="color: #e67e22; margin: 0 0 7px">Hola Poketrainer!</h2>
+				<p style="margin: 2px; font-size: 15px">
+					Somos la comunidad Poketrainers Trujillo, una comunidad de Pokémon VGC que se encuentra en la ciudad de Trujillo Perú.<br>
+					Estando próxima la salida de Pokémon Sol y Luna en la comunidad estamos realizando una serie de actividades que nos preparara para su llegada, así que los invitamos a formar parte de la comunidad y a acompañarnos en esta nueva aventura en la región de Alola, donde muchos pokemon y aventuras nos esperan!<br>
+					Entre las actividades tenemos:</p>
+				<ul style="font-size: 15px;  margin: 10px 0">
+					<li>Batallas amistosas.</li>
+					<li>Torneos Oficiales.</li>
+					<li>Intercambios de Pokémon.</li>
+					<li>Actividades de integración.</li>
+					<li>Muchas sorpresas más.</li>
+				</ul>
+				<div style="width: 100%; margin:20px 0; display: inline-block;text-align: center">
+<div style="width: 80px; display:inline-block">
+        <a href="https://www.facebook.com/UniversidaddelValledeMexico" target="_blank">
+        <img style="padding: 0; width: 50px; margin: 5px" src="https://image.ibb.co/fDmJRH/Facebook.png">
+        </a>
+</div>
+        <div style="width: 80px; display:inline-block">
+        <a href="https://twitter.com/UVMMEXICO" target="_blank">
+          <img style="padding: 0; width: 50px; margin: 5px" src="https://image.ibb.co/gbWGfc/twitter.png">
+        </a>
+        </div>
 
-      }
-    </style>
-  </head>
-  <body style="position: fixed; ">
-
-<img src="/img/mail_top.png" width="80%" height="400px"style="position: relative;" >
-
-<img src="/img/mail_bottom.jpg" width="80%" height="400px" style="margin-top: 200px; position: relative; margin-left: 0px;" >
-
-<img src="/img/mail_logo.png" width="200px" style="position: relative; margin-top: 10px; margin-left: 520px;">
-
-  <div id=content>
-
+        <div style="width: 80px; display:inline-block">
+          <a href="https://www.linkedin.com/school/universidad-del-valle-de-m-xico/" target="_blank">
+            <img style="padding: 0; width: 50px; margin: 5px" src="https://image.ibb.co/f5t7mH/linked.png">
+        </a>
   </div>
 
+  <div style="width: 80px;display:inline-block">
+          <a href="https://www.youtube.com/user/uvmmexico" target="_blank">
+            <img style="padding: 0; width: 50px; margin: 5px" src="https://image.ibb.co/noR4Yx/youtube.png">
+          </a>
+</div>
 
-  </body>
-</html>
+<div style="width: 80px; display:inline-block">
+          <a href="https://blog.universidaduvm.mx" target="_blank">
+            <img style="padding: 0; width: 50px; margin: 5px" src="https://image.ibb.co/cd1cmH/Blog.png">
+          </a>
+</div>
+
+
+
+
+
+
+        </div>
+				<div style="width: 100%; text-align: center">
+					<a style="text-decoration: none; border-radius: 5px; padding: 11px 23px; color: white; background-color: #3498db" href="https://www.facebook.com/PokemonTrujillo/">Ir a la página</a>
+				</div>
+				<p style="color: #b3b3b3; font-size: 12px; text-align: center;margin: 30px 0 0">Poketrainers Trujillo 2016</p>
+			</div>
+		</td>
+	</tr>
+</table>
+<!--hasta aquí-->
+
+</body>
+</html>S
--- resources/views/administrator/correo.blade.php
+++ resources/views/administrator/correo.blade.php
@@ -1,42 +1,84 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>UVM | Universidad del Valle de México </title>
-    <style>
-      #content
-      {
-        position: fixed;
-        margin-top: 200px;
-        overflow: auto;
-        word-break: break-all;
-        -webkit-hyphens: auto;
-        -moz-hyphens: auto;
-        hyphens: auto;
-        width: 80%;
-        height: 380px;
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>UVM | Universidad del Valle de México</title>
+</head>
+<body style="background-image: url('https://image.ibb.co/ej9skc/mail_center.png') ">
+<!--Copia desde aquí-->
+<table style="max-width: 800px; padding: 10px; margin:0 auto; border-collapse: collapse;">
+	<tr>
+		<td style="background-color: #ecf0f1; text-align: left; padding: 0">
+				<img width="20%" style="display:block; margin: 1.5% 3%" height="80px" src="https://image.ibb.co/eXzEyx/mail_logo.png">
+		</td>
+	</tr>
+	<tr>
+		<td style="padding: 0">
+			<img style="padding: 0; display: block" src="https://image.ibb.co/fmTvOx/self_assessment.jpg" width="100%">
+		</td>
+	</tr>
+	<tr>
+		<td style="background-color: #ecf0f1">
+			<div style="color: #34495e; margin: 4% 10% 2%; text-align: justify;font-family: sans-serif">
+				<h2 style="color: #e67e22; margin: 0 0 7px">¡Hola!</h2>
+				<p style="margin: 2px; font-size: 15px">
+          <?php echo $instrucciones; ?>
+          </p>
+			<div style="width: 100%;margin:20px 0; display: inline-block;text-align: center">
+	<hr />
+				</div>
+				<div style="width: 100%; text-align: center">
 
-      }
-    </style>
-  </head>
+          <?php
+          if (isset($_SERVER['HTTPS'])) { ?>
+            <div style="margin-bottom: 20px;">
+        <a href="https://<?php echo $_SERVER["HTTP_HOST"]; ?>/surveyed/previewtem/{{$infousuario->idE}}"> Contesta aqui tu encuesta</a>
+        </div>
+          <?php
+          } else {
+          ?>
+          <div style="margin-bottom: 20px;">
+                          <a href="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/surveyed/previewtem/{{$infousuario->idE}}"> Contesta aqui tu encuesta</a>
+                        </div>
+            <?php
+          }
+          ?>
 
+          <div style="width: 80px; display:inline-block">
+                  <a href="https://www.facebook.com/UniversidaddelValledeMexico" target="_blank">
+                  <img style="padding: 0; width: 50px; margin: 5px" src="https://image.ibb.co/fDmJRH/Facebook.png">
+                  </a>
+          </div>
+                  <div style="width: 80px; display:inline-block">
+                  <a href="https://twitter.com/UVMMEXICO" target="_blank">
+                    <img style="padding: 0; width: 50px; margin: 5px" src="https://image.ibb.co/gbWGfc/twitter.png">
+                  </a>
+                  </div>
 
-  <body style="position: fixed; ">
-    <img src="https://image.ibb.co/n9ndQc/mail_top.png" width="80%" height="400px"style="position: fixed;" >
+                  <div style="width: 80px; display:inline-block">
+                    <a href="https://www.linkedin.com/school/universidad-del-valle-de-m-xico/" target="_blank">
+                      <img style="padding: 0; width: 50px; margin: 5px" src="https://image.ibb.co/f5t7mH/linked.png">
+                  </a>
+            </div>
 
-    <img src="https://image.ibb.co/cxugdx/mail_bottom.jpg" width="80%" height="400px" style="margin-top: 250px; position: fixed; margin-left: 0px;" >
+            <div style="width: 80px;display:inline-block">
+                    <a href="https://www.youtube.com/user/uvmmexico" target="_blank">
+                      <img style="padding: 0; width: 50px; margin: 5px" src="https://image.ibb.co/noR4Yx/youtube.png">
+                    </a>
+          </div>
 
-    <img src="https://image.ibb.co/eXzEyx/mail_logo.png" width="200px" style="position: fixed; margin-top: 10px; margin-left: 520px;">
+          <div style="width: 80px; display:inline-block">
+                    <a href="https://blog.universidaduvm.mx" target="_blank">
+                      <img style="padding: 0; width: 50px; margin: 5px" src="https://image.ibb.co/cd1cmH/Blog.png">
+                    </a>
+          </div>
+      	</div>
+				<p style="color: #b3b3b3; font-size: 12px; text-align: center;margin: 30px 0 0">D.R.© Universidad del Valle de México, México. <?php echo date("Y"); ?> Laureate International Universities</p>
+			</div>
+		</td>
+	</tr>
+</table>
+<!--hasta aquí-->
 
-
-
-  <div id=content>
-<?php echo $instrucciones; ?>
-  </div>
-<center>
-  <br />
-  <br>
- <a href="http://localhost:8000/surveyed/previewtem/{{$infousuario->idE}}"> Contesta aqui tu encuesta</a>
-</center>
-  </body>
-</html>
+</body>
+</html>S
