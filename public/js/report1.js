f812ef31e73248ce831e69698f1c6dd5cf2dcd23
AlexisGaleas94 <alexis.galeas93@gmail.com>
2018-03-01 15:48:44 -0600

    cambios2

--- public/js/report1.js
+++ public/js/report1.js
@@ -23,11 +23,32 @@ function sltlinea(busq){
               success : function(response){
                 console.log(response);
               var  dato="";
-              var ENC=""; FAL="";  Total="";  SIS="";  OLN=""  CNX  #FBK  #HP12 #EMAIL  #PRM  #DET  %NPS  %Avance
+              var ENC=0; FAL=0;  Total=0;  SIS=0;  OLN=0;  CNX=0;  FBK=0;  HP12=0; EMAIL=0;  PRM=0;  DET=0;  NPS=0;  Avance=0;
               var json=jQuery.parseJSON(JSON.stringify(response));
               var json2=jQuery.parseJSON(JSON.stringify(response));
               for(post in json2){
                 ENC++;
+                if(json[post].contestado==0){
+                  FAL++;
+                }
+                if(json[post].canal=="sistema"){
+                  SIS++;
+                }
+                if(json[post].canal=="online"){
+                  OLN++;
+                }
+                if(json[post].canal=="conexion"){
+                  CNX++;
+                }
+                if(json[post].canal=="facebook"){
+                  FBK++;
+                }
+                if(json[post].canal=="hp12c"){
+                  HP12++;
+                }
+                if(json[post].canal=="mail"){
+                  EMAIL++;
+                } 
 
               }
               for(post in json){
@@ -35,14 +56,14 @@ function sltlinea(busq){
                 dato+="<td>"+json[post].lineaNegocio+"</td>"
                 dato+="<td>"+json[post].modalidad+"</td>"
                 dato+="<td>"+ENC+"</td>"
-                dato+="<td>"+json[post].lineaNegocio+"</td>"
-                dato+="<td>"+json[post].lineaNegocio+"</td>"
-                dato+="<td>"+json[post].lineaNegocio+"</td>"
-                dato+="<td>"+json[post].lineaNegocio+"</td>"
-                dato+="<td>"+json[post].lineaNegocio+"</td>"
-                dato+="<td>"+json[post].lineaNegocio+"</td>"
-                dato+="<td>"+json[post].lineaNegocio+"</td>"
-                dato+="<td>"+json[post].lineaNegocio+"</td>"
+                dato+="<td>"+FAL+"</td>"
+                dato+="<td>"+ENC+"</td>"
+                dato+="<td>"+SIS+"</td>"
+                dato+="<td>"+OLN+"</td>"
+                dato+="<td>"+CNX+"</td>"
+                dato+="<td>"+FBK+"</td>"
+                dato+="<td>"+HP12+"</td>"
+                dato+="<td>"+EMAIL+"</td>"
                 dato+="<td>"+json[post].lineaNegocio+"</td>"
                 dato+="<td>"+json[post].lineaNegocio+"</td>"
                 dato+="<td>"+json[post].lineaNegocio+"</td>"
