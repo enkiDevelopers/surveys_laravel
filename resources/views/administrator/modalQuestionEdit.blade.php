a249981ef5c6532c92bdc0d4e0a47ef09c47f0cb
Jorge Gonzalez <colocho-2104@hotmail.com>
2018-03-16 21:20:26 -0600

    modal

--- resources/views/administrator/modalQuestionEdit.blade.php
+++ resources/views/administrator/modalQuestionEdit.blade.php
@@ -77,7 +77,131 @@
             </div>
         </div>
     </div>
-
- 
 </form>
 
+<!--################################## nuevo modal   ###################################################-->
+
+
+<form id="myForm" method="post" action="/save" enctype="multipart/form-data" id="myForm">
+        {{ csrf_field() }}
+<div class="modal fade bd-example-modal-lg" id="ModalTitle" aria-hidden="true" data-keyboard="false" data-backdrop="static" tabindex="-1" style="z-index: 1050;" role ="dialog" aria-labelledby="myModalLabel1">
+  <div class="modal-dialog modal-lg" role="document">
+
+      <div class="modal-content">
+      <div class="modal-header">
+<div class="row">
+<div class="col-sm-10">
+<h4 class="modal-title" id="myModalLabel1">Inicializar plantila</h4>
+</div>
+
+<div class="col-sm-2 pull-right pull-right">
+<div class="pull-right">
+<a type="button" class="glyphicon glyphicon-remove" data-dismiss="modal" href="administrator/surveys" onclick="limpiar()"></a>
+      </div>
+      </div>
+
+      </div>
+      </div>
+
+      <div class="modal-body">
+        <input type="hidden" value="{{$id}}" name="creador" id="creador"/>
+          <input type="hidden" id="token" name="_token" value="{{ csrf_token() }}"/>
+
+          <table id="aPlantilla" class="table">
+          <tr>
+              <td style="width:50%">
+              <div style="width:100%;">
+                <input
+                type="text" maxlength="40" class="form-control text-black "
+                required id="ModalTitleInput" aria-describedby="emailHelp"
+                placeholder="Titulo de la encuesta" name="titulo" style="margin-bottom: 20px;"/>
+                </div>
+              </td>
+                    <td style="text-align: center;">
+              <input type="file" id="foto1" name="icono" onclick="limpiar2();"/>
+                    </td>
+          </tr>
+              <tr>
+                <td>
+
+                  <textarea maxlength="500"
+                  class="form-control text-black" required
+                  cols="10" rows="5" name="descripcion" id="ModalDescInput" aria-describedby="desc"
+                  placeholder="Descripción del proposito de la encuesta"></textarea>
+
+                </td>
+                    <td>
+                          <div id="imgC">
+                          <img id="imgSalida" src="/img/iconos/default.png"/>
+                              </div>
+                    </td>
+              </tr>
+          </table>
+
+
+      </div>
+
+
+      <div class="modal-footer">
+
+   <a type="button" class="btn btn-default" data-dismiss="modal"
+   onclick="limpiar()">Cancelar</a>
+          <button type="submit" class="btn btn-primary">Guardar</button>
+      </div>
+      </div>
+
+
+
+  </div>
+
+
+</div>
+</form>
+
+
+<script>
+
+function limpiar(){
+$('#imgSalida').attr("src","/img/iconos/default.png");
+  document.getElementById("myForm").reset();
+
+                  }
+      function limpiar2()
+                {
+                  $('#imgSalida').attr("src","/img/iconos/default.png");
+                  }
+
+
+
+                  function imagen(){
+                       $('#foto1').change(function(e) {
+                           addImage(e);
+                          });
+
+                          function addImage(e){
+                           var file = e.target.files[0],
+                           imageType = /image.*/;
+
+                           if (!file.type.match(imageType))
+                           {
+                             swal({
+                                title: "Su archivo no es una imagen",
+                                type: "error",
+                                 });
+
+                                 return;
+                           }
+                           var reader = new FileReader();
+                           reader.onload = fileOnload;
+                           reader.readAsDataURL(file);
+                          }
+
+                          function fileOnload(e) {
+                           var result=e.target.result;
+                           $('#imgSalida').attr("src",result);
+                          }
+
+                       }
+
+
+</script>
