97df17a663d77a3ea7b1b28635607b2d77416da6
Juan Carlos Lopez Pimentel <jclopezpimentel@gmail.com>
2018-05-29 11:48:40 -0500

    comentario 2
    Comentario a las rutas de web services

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 */
 
 //Agregando web services
+//Este comentario es temporal
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
