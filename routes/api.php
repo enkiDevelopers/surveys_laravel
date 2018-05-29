52bdfe772b5ce5f6db91bb533af528bc724aaa41
Juan Carlos Lopez Pimentel <jclopezpimentel@gmail.com>
2018-05-29 14:46:02 -0500

    comentario api
    Se agrega un comentario a api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 */
 
 //Agregando web services
+//Agregando un comentario
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
