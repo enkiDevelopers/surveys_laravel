b56313c86fc7164813e5950712e14da7a6102113
Juan Carlos Lopez Pimentel <jclopezpimentel@gmail.com>
2018-05-29 14:47:00 -0500

    modificación al comentario
    Se ha comentado nuevamente

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 //Agregando web services
-//Agregando un comentario
+//Agregando un comentar
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
