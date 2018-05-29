133934ab0db0d466cf24476aa741def7492a0f49
Juan Carlos Lopez Pimentel <jclopezpimentel@gmail.com>
2018-05-29 11:49:37 -0500

    Revert "comentario 2"
    This reverts commit 97df17a663d77a3ea7b1b28635607b2d77416da6.

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,6 @@ use Illuminate\Http\Request;
 */
 
 //Agregando web services
-//Este comentario es temporal
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
